94a24eaba52c2dc026be662f1d7b607ac4cfaf15
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-06-23 18:40:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        noseX = results[0].pose.nose.x - 17;
-        noseY =  results[0].pose.nose.y - 20;
+        noseX = results[0].pose.nose.x - 13;
+        noseY =  results[0].pose.nose.y - 13;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
     }
